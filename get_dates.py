c174b0565e7392246ed34da62449ec00d77c0941
JeremyHudson43 <jer4349@gmail.com>
2023-02-05 17:00:22 -0500

    Update get_dates.py

--- get_dates.py
+++ get_dates.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import os
 import pandas as pd
+from datetime import datetime
 
 base_path = r'C:\Users\jer43\OneDrive\Desktop\cash_5'
 all_files = os.listdir(base_path)
@@ -20,7 +21,13 @@ for file_path in all_files:
         if len(tds) == 4 and 'Double Play' not in tds[2].text:
             date = tds[0].text.strip()
             balls = [ball.text for ball in tds[1].find_all('span', class_='ball')]
-            payout = tds[2].text.strip()
+
+            payout = tds[2].text.strip().replace('$', '')
+            payout = int(payout)
+
+            date_object = datetime.strptime(date, '%Y %b %d')
+            date = date_object.strftime('%Y-%m-%d')
+            
             if 'Double' not in payout:
                 data.append({'date': date, 'balls': balls, 'payout': payout})
 
